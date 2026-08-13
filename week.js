a5714bd35b846a5d348851f4726a42f2390bcbc1
joshuabrownsc <cgxdfsg2mv@privaterelay.appleid.com>
2026-08-13 13:26:08 -0400

    Update week.js

--- week.js
+++ week.js
@@ -87,10 +87,10 @@ psalm: {
   announcements: [
     { title: "Midweek This Tuesday at 7 PM",
       body: "We will be continuing in Acts 5. Check GroupMe for all details." },
-    { title: "Fireflies Game | Saturday, August 22",
-      body: "We are going to the Columbia Fireflies game as a church for Faith and Family Night. Purchase tickets here:
-      link: "https://www.gofevo.com/event/Dreamchurch2"
-      linkLabel: "Get Tickets"}
+{ title: "Fireflies Game | Saturday, August 22",
+      body: "We are going to the Columbia Fireflies game as a church for Faith and Family Night. Purchase tickets here:",
+      link: "https://www.gofevo.com/event/Dreamchurch2",
+      linkLabel: "Get Tickets" }
   ],
 
 
